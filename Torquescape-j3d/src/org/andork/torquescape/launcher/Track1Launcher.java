6389945aa7302a6fa8ac4fd21811bd06f5c9eeac
Andy Edwards <jedwards8@gmail.com>
2013-07-22 23:38:58 -0500

    Created Torquescape-j3d project

---
+++ Torquescape-j3d/src/org/andork/torquescape/launcher/Track1Launcher.java
@@ -0,0 +1,11 @@
+package org.andork.torquescape.launcher;
+
+import org.andork.torquescape.track.Track1;
+
+public class Track1Launcher
+{
+	public static void main( String[ ] args )
+	{
+		TorquescapeLauncher.launch( new Track1( ) );
+	}
+}
