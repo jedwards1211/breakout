2a99a610720fb58eec39e862d769f25d383c586d
Andy Edwards <jedwards8@gmail.com>
2014-01-06 16:05:06 -0600

    Created utilities for filtering tables

---
+++ andork-awt/src/org/andork/awt/FilteringTableController.java
@@ -0,0 +1,59 @@
+package org.andork.awt;
+
+import java.awt.Color;
+import java.util.regex.Pattern;
+
+import javax.swing.JTable;
+import javax.swing.event.DocumentEvent;
+import javax.swing.text.JTextComponent;
+
+import org.andork.awt.FilteringTableModel.Filter;
+import org.andork.awt.FilteringTableModel.PatternFilter;
+
+public class FilteringTableController
+{
+	JTextComponent	regexField;
+	JTable			table;
+	
+	public FilteringTableController( JTextComponent regexField , JTable table )
+	{
+		super( );
+		this.regexField = regexField;
+		this.table = table;
+		
+		regexField.getDocument( ).addDocumentListener( new EasyDocumentListener( )
+		{
+			@Override
+			public void documentChanged( DocumentEvent e )
+			{
+				onRegexChanged( );
+			}
+		} );
+	}
+	
+	protected void onRegexChanged( )
+	{
+		Pattern p;
+		try
+		{
+			p = Pattern.compile( regexField.getText( ) );
+			regexField.setForeground( Color.BLACK );
+		}
+		catch( Exception ex )
+		{
+			regexField.setForeground( Color.RED );
+			return;
+		}
+		
+		if( table.getModel( ) instanceof FilteringTableModel )
+		{
+			FilteringTableModel model = ( FilteringTableModel ) table.getModel( );
+			model.setFilter( createFilter( p ) );
+		}
+	}
+	
+	private Filter createFilter( Pattern p )
+	{
+		return new PatternFilter( p );
+	}
+}
---
+++ andork-awt/test/org/andork/awt/TextComponentWithHintAndClearTest.java
@@ -0,0 +1,9 @@
+package org.andork.awt;
+
+public class TextComponentWithHintAndClearTest
+{
+	public static void main( String[ ] args )
+	{
+		QuickTestFrame.frame( new TextComponentWithHintAndClear( "Enter filter pattern" ) ).setVisible( true );
+	}
+}
