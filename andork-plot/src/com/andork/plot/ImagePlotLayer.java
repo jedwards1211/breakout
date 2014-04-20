5d711155e3ffc1c4af860228a38061fc7963230e
Andy Edwards <jedwards8@gmail.com>
2014-04-20 11:27:02 -0500

    Created ImagePlotLayer/ImagePlotPanel

---
+++ andork-plot/src/com/andork/plot/ImagePlotLayer.java
@@ -0,0 +1,104 @@
+package com.andork.plot;
+
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Rectangle;
+import java.awt.RenderingHints;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Rectangle2D;
+
+public class ImagePlotLayer implements IPlotLayer
+{
+	LinearAxisConversion	xAxisConversion;
+	LinearAxisConversion	yAxisConversion;
+	
+	Rectangle2D				src;
+	Rectangle2D				dest;
+	
+	Image					image;
+	
+	public ImagePlotLayer( LinearAxisConversion xAxisConversion , LinearAxisConversion yAxisConversion )
+	{
+		super( );
+		this.xAxisConversion = xAxisConversion;
+		this.yAxisConversion = yAxisConversion;
+	}
+	
+	public LinearAxisConversion getxAxisConversion( )
+	{
+		return xAxisConversion;
+	}
+	
+	public void setxAxisConversion( LinearAxisConversion xAxisConversion )
+	{
+		this.xAxisConversion = xAxisConversion;
+	}
+	
+	public LinearAxisConversion getyAxisConversion( )
+	{
+		return yAxisConversion;
+	}
+	
+	public void setyAxisConversion( LinearAxisConversion yAxisConversion )
+	{
+		this.yAxisConversion = yAxisConversion;
+	}
+	
+	public Rectangle2D getSrc( )
+	{
+		return src;
+	}
+	
+	public void setSrc( Rectangle2D src )
+	{
+		this.src = src;
+	}
+	
+	public Rectangle2D getDest( )
+	{
+		return dest;
+	}
+	
+	public void setDest( Rectangle2D dest )
+	{
+		this.dest = dest;
+	}
+	
+	public Image getImage( )
+	{
+		return image;
+	}
+	
+	public void setImage( Image image )
+	{
+		this.image = image;
+	}
+	
+	@Override
+	public void render( Graphics2D g2 , Rectangle bounds )
+	{
+		if( image == null || xAxisConversion == null || yAxisConversion == null || src == null || dest == null )
+		{
+			return;
+		}
+		
+		double dx = xAxisConversion.convert( dest.getX( ) );
+		double dy = yAxisConversion.convert( dest.getY( ) );
+		double dw = xAxisConversion.convert( dest.getMaxX( ) ) - dx;
+		double dh = yAxisConversion.convert( dest.getMaxY( ) ) - dy;
+		
+		int sx1 = ( int ) Math.floor( src.getX( ) );
+		int sx2 = ( int ) Math.ceil( src.getMaxX( ) );
+		int sy1 = ( int ) Math.floor( src.getY( ) );
+		int sy2 = ( int ) Math.ceil( src.getMaxY( ) );
+		
+		AffineTransform prevXform = g2.getTransform( );
+		
+		
+		g2.translate( dx - src.getX( ) , dy - src.getY( ) );
+		g2.scale( dw / src.getWidth( ) , dh / src.getHeight( ) );
+		g2.drawImage( image , sx1 , sy1 , sx2 , sy2 , sx1 , sy1 , sx2 , sy2 , null );
+		
+		g2.setTransform( prevXform );
+	}
+}
---
+++ andork-plot/src/com/andork/plot/ImagePlotPanel.java
@@ -0,0 +1,76 @@
+package com.andork.plot;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Image;
+import java.awt.geom.Rectangle2D;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import org.andork.awt.layout.Corner;
+
+import com.andork.plot.PlotAxis.LabelPosition;
+import com.andork.plot.PlotAxis.Orientation;
+
+@SuppressWarnings( "serial" )
+public class ImagePlotPanel extends JPanel
+{
+	Plot				plot;
+	ImagePlotLayer		imagePlotLayer;
+	PlotAxis			xAxis;
+	PlotAxis			yAxis;
+	AxisLinkButton		axisLinkButton;
+	
+	PlotAxisController	xAxisController;
+	PlotAxisController	yAxisController;
+	PlotController		plotController;
+	MouseLooper			mouseLooper;
+	
+	public ImagePlotPanel( )
+	{
+		plot = new Plot( );
+		xAxis = new PlotAxis( Orientation.HORIZONTAL , LabelPosition.TOP );
+		yAxis = new PlotAxis( Orientation.VERTICAL , LabelPosition.LEFT );
+		
+		xAxis.addPlot( plot );
+		yAxis.addPlot( plot );
+		
+		xAxisController = new PlotAxisController( xAxis );
+		yAxisController = new PlotAxisController( yAxis );
+		plotController = new PlotController( plot , xAxisController , yAxisController );
+		
+		axisLinkButton = new AxisLinkButton( xAxisController , yAxisController );
+		
+		imagePlotLayer = new ImagePlotLayer( xAxis.getAxisConversion( ) , yAxis.getAxisConversion( ) );
+		plot.addLayer( imagePlotLayer );
+		
+		setLayout( new PlotPanelLayout( ) );
+		
+		add( plot );
+		add( xAxis );
+		add( yAxis );
+		add( axisLinkButton , Corner.TOP_LEFT );
+		
+		mouseLooper = new MouseLooper( );
+		mouseLooper.addMouseAdapter( plotController );
+		addMouseListener( mouseLooper );
+		addMouseMotionListener( mouseLooper );
+		addMouseWheelListener( mouseLooper );
+	}
+	
+	public Image getImage( )
+	{
+		return imagePlotLayer.getImage( );
+	}
+	
+	public void setImage( Image image )
+	{
+		imagePlotLayer.setImage( image );
+		imagePlotLayer.setSrc( new Rectangle2D.Double( 0.0 , 0.0 , image.getWidth( null ) , image.getHeight( null ) ) );
+		imagePlotLayer.setDest( imagePlotLayer.getSrc( ) );
+		plot.repaint( );
+	}
+}
