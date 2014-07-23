c0a766210e25017c9335f87ef2e6c3f90003f45f
U-hexagon-sun\Andy <Andy@hexagon-sun.(none)>
2014-07-22 23:02:11 -0500

    removing Torquescape stuff

--- torquescape-jogl-gl2es2/src/org/andork/torquescape/jogl/render/ISliceRendererFactory.java
+++
@@ -1,8 +0,0 @@
-package org.andork.torquescape.jogl.render;
-
-import org.andork.torquescape.model.ISlice;
-
-public interface ISliceRendererFactory<S extends ISlice>
-{
-	public ISliceRenderer<S> create( ZoneRenderer zoneRenderer , S slice );
-}
