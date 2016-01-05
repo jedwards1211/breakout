e3cdd6a6a480ffa8cd55d8b3a96574266f3d6b96
Andy Edwards <jedwards@fastmail.com>
2016-01-05 13:55:30 -0600

    started down this path until I realized maven multi-module projects are gross because a module can't be used in multiple multi-module projects

--- breakout-lib/package.sh
+++ breakout-lib/package.sh
@@ -10,6 +10,6 @@ cd "${BASE_DIR}/andork-ui" 					&& mvn install &&
 cd "${BASE_DIR}/andork-ui-test" 			&& mvn install &&
 cd "${BASE_DIR}/andork-plot" 				&& mvn install &&
 cd "${BASE_DIR}/andork-spatial" 			&& mvn install &&
-cd "${BASE_DIR}/andork-jogl-gl2es2-utils" 	&& mvn install &&
-cd "${BASE_DIR}/andork-jogl-gl2es2-swing" 	&& mvn install &&
+cd "${BASE_DIR}/andork-jogl-utils" 	&& mvn install &&
+cd "${BASE_DIR}/andork-jogl-swing" 	&& mvn install &&
 cd "${BASE_DIR}/Breakout" 					&& mvn package
