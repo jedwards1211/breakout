232c3275c066067ca4a5cca0222183be9077b3d4
Andy Edwards <jedwards@fastmail.com>
2016-10-22 12:05:20 -0500

    more work on new survey table and compass converter

---
+++ Breakout/src/main/java/org/breakout/RawNumberCellRenderer.java
@@ -0,0 +1,43 @@
+package org.breakout;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.text.NumberFormat;
+import java.text.ParseException;
+
+import javax.swing.JLabel;
+import javax.swing.JTable;
+import javax.swing.SwingConstants;
+import javax.swing.table.DefaultTableCellRenderer;
+
+public class RawNumberCellRenderer extends DefaultTableCellRenderer {
+	private static final long serialVersionUID = 3115504068270453371L;
+
+	private final NumberFormat numberFormat;
+
+	public RawNumberCellRenderer(NumberFormat format) {
+		numberFormat = format;
+	}
+
+	@Override
+	public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus,
+			int row, int column) {
+		boolean invalid = false;
+		if (value != null) {
+			String str = "";
+			try {
+				str = value.toString();
+				value = numberFormat.format(numberFormat.parse(str));
+			} catch (ParseException e) {
+				if (!str.isEmpty()) {
+					invalid = true;
+				}
+			}
+		}
+		JLabel label = (JLabel) super.getTableCellRendererComponent(
+				table, value, isSelected, hasFocus, row, column);
+		label.setHorizontalAlignment(SwingConstants.RIGHT);
+		label.setBackground(invalid ? new Color(255, 64, 64) : null);
+		return label;
+	}
+}
