0198c1848a8112598f179ab19d8583bbcf1418f5
Andy Edwards <jedwards@fastmail.com>
2016-10-24 17:43:32 -0500

    add some tests

--- Breakout/src/main/java/org/breakout/compass/CompassConverter.java
+++ Breakout/src/main/java/org/breakout/compass/CompassConverter.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.andork.compass.CompassShot;
 import org.andork.compass.CompassTrip;
+import org.andork.compass.CompassTripHeader;
 import org.andork.compass.LengthUnit;
 import org.andork.compass.LrudAssociation;
 import org.breakout.model.NewSurveyTableModel.Row;
@@ -15,55 +16,15 @@ public class CompassConverter {
 	public static List<Row> convertFromCompass(List<CompassTrip> compassTrips) {
 		List<Row> shots = new ArrayList<Row>();
 
-		LengthUnit lengthUnit = LengthUnit.METERS;
-		for (LengthUnit unit : LengthUnit.values()) {
-			if (compassTrips.stream().allMatch(trip -> trip.getHeader().getLengthUnit() == unit)) {
-				lengthUnit = unit;
-			}
-		}
+		LengthUnit lengthUnit = pickLengthUnit(compassTrips);
 
 		for (CompassTrip compassTrip : compassTrips) {
-			Trip trip = new Trip();
-			trip.setCave(compassTrip.getHeader().getCaveName());
-			trip.setName(compassTrip.getHeader().getComment());
-			trip.setDate(toString(compassTrip.getHeader().getDate()));
-			trip.setSurveyors(toString(compassTrip.getHeader().getTeam()));
+			Trip trip = convertTripHeader(compassTrip.getHeader());
 
-			List<CompassShot> compassShots = compassTrip.getShots();
-			List<Row> tripShots = new ArrayList<Row>();
+			List<Row> tripShots = convertShots(compassTrip, lengthUnit);
 
-			for (CompassShot compassShot : compassTrip.getShots()) {
-				Row shot = new Row();
+			for (Row shot : tripShots) {
 				shot.setTrip(trip);
-				shot.setFromStation(compassShot.getFromStationName());
-				shot.setToStation(compassShot.getToStationName());
-				shot.setDistance(toString(LengthUnit.convert(compassShot.getLength(), lengthUnit)));
-				shot.setFrontAzimuth(toString(compassShot.getFrontsightAzimuth()));
-				shot.setBackAzimuth(toString(compassShot.getBacksightAzimuth()));
-				shot.setFrontInclination(toString(compassShot.getFrontsightInclination()));
-				shot.setBackInclination(toString(compassShot.getBacksightInclination()));
-				tripShots.add(shot);
-			}
-
-			Iterator<Row> tripShotIter = tripShots.iterator();
-			if (compassTrip.getHeader().getLrudAssociation() == LrudAssociation.TO && !compassShots.isEmpty()) {
-				// add a row for the LRUDs at the to station of the last shot
-				Row shot = new Row();
-				shot.setTrip(trip);
-				shot.setFromStation(last(compassShots).getToStationName());
-
-				// offset tripShotIter so that compass LRUDs for to station get
-				// applied to (from station of) *next* shot
-				tripShotIter.next();
-			}
-			Iterator<CompassShot> compassShotIter = compassShots.iterator();
-			while (tripShotIter.hasNext() && compassShotIter.hasNext()) {
-				Row shot = tripShotIter.next();
-				CompassShot compassShot = compassShotIter.next();
-				shot.setLeft(toString(compassShot.getLeft()));
-				shot.setRight(toString(compassShot.getRight()));
-				shot.setUp(toString(compassShot.getUp()));
-				shot.setDown(toString(compassShot.getDown()));
 			}
 
 			shots.addAll(tripShots);
@@ -72,8 +33,70 @@ public class CompassConverter {
 		return shots;
 	}
 
+	public static List<Row> convertShots(CompassTrip compassTrip, LengthUnit lengthUnit) {
+		List<CompassShot> compassShots = compassTrip.getShots();
+		List<Row> tripShots = new ArrayList<Row>();
+
+		if (compassShots == null) {
+			return tripShots;
+		}
+		for (CompassShot compassShot : compassTrip.getShots()) {
+			Row shot = new Row();
+			shot.setFromStation(compassShot.getFromStationName());
+			shot.setToStation(compassShot.getToStationName());
+			shot.setDistance(toString(LengthUnit.convert(compassShot.getLength(), lengthUnit)));
+			shot.setFrontAzimuth(toString(compassShot.getFrontsightAzimuth()));
+			shot.setBackAzimuth(toString(compassShot.getBacksightAzimuth()));
+			shot.setFrontInclination(toString(compassShot.getFrontsightInclination()));
+			shot.setBackInclination(toString(compassShot.getBacksightInclination()));
+			tripShots.add(shot);
+		}
+
+		Iterator<Row> tripShotIter = tripShots.iterator();
+		if (compassTrip.getHeader().getLrudAssociation() == LrudAssociation.TO && !compassShots.isEmpty()) {
+			// add a row for the LRUDs at the to station of the last shot
+			Row shot = new Row();
+			shot.setFromStation(last(compassShots).getToStationName());
+			tripShots.add(shot);
+
+			// offset tripShotIter so that compass LRUDs for to station get
+			// applied to (from station of) *next* shot
+			tripShotIter = tripShots.iterator();
+			tripShotIter.next();
+		}
+		Iterator<CompassShot> compassShotIter = compassShots.iterator();
+		while (tripShotIter.hasNext() && compassShotIter.hasNext()) {
+			Row shot = tripShotIter.next();
+			CompassShot compassShot = compassShotIter.next();
+			shot.setLeft(toString(compassShot.getLeft()));
+			shot.setRight(toString(compassShot.getRight()));
+			shot.setUp(toString(compassShot.getUp()));
+			shot.setDown(toString(compassShot.getDown()));
+		}
+		return tripShots;
+	}
+
+	public static Trip convertTripHeader(CompassTripHeader compassTripHeader) {
+		Trip trip = new Trip();
+		trip.setCave(compassTripHeader.getCaveName());
+		trip.setName(compassTripHeader.getComment());
+		trip.setDate(toString(compassTripHeader.getDate()));
+		trip.setSurveyors(toString(compassTripHeader.getTeam()));
+		return trip;
+	}
+
 	private static <T> T last(List<T> list) {
 		return list.listIterator(list.size()).previous();
+	}
+
+	public static LengthUnit pickLengthUnit(List<CompassTrip> compassTrips) {
+		LengthUnit lengthUnit = LengthUnit.METERS;
+		for (LengthUnit unit : LengthUnit.values()) {
+			if (compassTrips.stream().allMatch(trip -> trip.getHeader().getLengthUnit() == unit)) {
+				lengthUnit = unit;
+			}
+		}
+		return lengthUnit;
 	}
 
 	private static String toString(double value) {
---
+++ Breakout/src/test/java/org/breakout/compass/CompassConverterTest.java
@@ -0,0 +1,99 @@
+package org.breakout.compass;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.andork.compass.CompassShot;
+import org.andork.compass.CompassTrip;
+import org.andork.compass.CompassTripHeader;
+import org.andork.compass.LengthUnit;
+import org.andork.compass.LrudAssociation;
+import org.breakout.model.NewSurveyTableModel.Row;
+import org.breakout.model.NewSurveyTableModel.Trip;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CompassConverterTest {
+	@Test
+	public void testEmptyShot() {
+		CompassTripHeader header = new CompassTripHeader();
+		CompassTrip trip = new CompassTrip();
+		trip.setHeader(header);
+		CompassShot shot = new CompassShot();
+		trip.setShots(Arrays.asList(shot));
+
+		List<Row> rows = CompassConverter.convertShots(trip, LengthUnit.METERS);
+		Assert.assertEquals(1, rows.size());
+		Row row = rows.get(0);
+		Assert.assertNull(row.getFromCave());
+		Assert.assertNull(row.getFromStation());
+		Assert.assertNull(row.getToCave());
+		Assert.assertNull(row.getToStation());
+		Assert.assertNull(row.getDistance());
+		Assert.assertNull(row.getFrontAzimuth());
+		Assert.assertNull(row.getFrontInclination());
+		Assert.assertNull(row.getBackAzimuth());
+		Assert.assertNull(row.getBackInclination());
+		Assert.assertNull(row.getLeft());
+		Assert.assertNull(row.getRight());
+		Assert.assertNull(row.getUp());
+		Assert.assertNull(row.getDown());
+		Assert.assertNull(row.getNorthing());
+		Assert.assertNull(row.getEasting());
+		Assert.assertNull(row.getElevation());
+	}
+
+	@Test
+	public void testEmptyTrip() {
+		CompassTrip trip = new CompassTrip();
+		trip.setHeader(new CompassTripHeader());
+		List<Row> rows = CompassConverter.convertShots(trip, LengthUnit.METERS);
+		Assert.assertEquals(0, rows.size());
+	}
+
+	@Test
+	public void testEmptyTripHeader() {
+		CompassTripHeader header = new CompassTripHeader();
+		Trip trip = CompassConverter.convertTripHeader(header);
+		Assert.assertNull(trip.getCave());
+		Assert.assertNull(trip.getName());
+		Assert.assertNull(trip.getSurveyors());
+		Assert.assertNull(trip.getDate());
+	}
+
+	@Test
+	public void testFromLruds() {
+		CompassTripHeader header = new CompassTripHeader();
+		header.setLrudAssociation(LrudAssociation.FROM);
+		CompassTrip trip = new CompassTrip();
+		trip.setHeader(header);
+		CompassShot shot = new CompassShot();
+		shot.setLeft(3.5);
+		trip.setShots(Arrays.asList(shot));
+
+		List<Row> rows = CompassConverter.convertShots(trip, LengthUnit.METERS);
+		Assert.assertEquals(1, rows.size());
+		Assert.assertEquals("3.5", rows.get(0).getLeft());
+	}
+
+	@Test
+	public void testToLruds() {
+		CompassTripHeader header = new CompassTripHeader();
+		header.setLrudAssociation(LrudAssociation.TO);
+		CompassTrip trip = new CompassTrip();
+		trip.setHeader(header);
+		CompassShot shot = new CompassShot();
+		shot.setFromStationName("A1");
+		shot.setToStationName("A2");
+		shot.setLeft(3.5);
+		trip.setShots(Arrays.asList(shot));
+
+		List<Row> rows = CompassConverter.convertShots(trip, LengthUnit.METERS);
+		Assert.assertEquals(2, rows.size());
+		Assert.assertEquals(null, rows.get(0).getLeft());
+		Assert.assertEquals("A1", rows.get(0).getFromStation());
+		Assert.assertEquals("A2", rows.get(0).getToStation());
+		Assert.assertEquals("A2", rows.get(1).getFromStation());
+		Assert.assertEquals("3.5", rows.get(1).getLeft());
+	}
+}
