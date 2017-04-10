b9ec0550216c9c0df4e14d551b016c7c19f3a9be
Andy Edwards <jedwards@fastmail.com>
2017-04-09 19:16:35 -0500

    fix(Rectmath): fix bug in intersects()

--- andork-spatial/src/org/andork/spatial/Rectmath.java
+++ andork-spatial/src/org/andork/spatial/Rectmath.java
@@ -144,7 +144,7 @@ public class Rectmath {
 		int d = r1.length / 2;
 		for (int i = 0; i < d; i++) {
 			int j = i + d;
-			if (r1[i] > r2[i] || r1[j] < r2[j]) {
+			if (r1[i] > r2[j] || r1[j] < r2[i]) {
 				return false;
 			}
 		}
@@ -155,7 +155,7 @@ public class Rectmath {
 		int d = r1.length / 2;
 		for (int i = 0; i < d; i++) {
 			int j = i + d;
-			if (r1[i] > r2[i] || r1[j] < r2[j]) {
+			if (r1[i] > r2[j] || r1[j] < r2[i]) {
 				return false;
 			}
 		}
