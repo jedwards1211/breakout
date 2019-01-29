6e9cc71d3e940a3b5582bf587920250a56780325
Andy Edwards <jedwards@fastmail.com>
2019-01-29 01:55:37 -0600

    build(build.sh): copy to /Applications on local machine

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 mvn clean install -P osx,windows,windows-i586,windows-amd64,linux,linux-i586,linux-amd64
+if [[ -e /Applications ]]; then
+	VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -Ev '(^\[|Download\w+:)')
+  	rm /Applications/breakout-*.jar
+	cp breakout/target/breakout-$VERSION.jar /Applications
+	ln -sf /Applications/breakout-$VERSION.jar /Applications/Breakout
+fi
