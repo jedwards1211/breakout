cd9f10c23ed22f4f7bbd0f5e921e94a2e27d0f81
Andy Edwards <jedwards@fastmail.com>
2019-01-29 13:40:53 -0600

    build: don't remove old versions from /Applications

--- build.sh
+++ build.sh
@@ -3,7 +3,6 @@
 mvn clean install -P osx,windows,windows-i586,windows-amd64,linux,linux-i586,linux-amd64
 if [[ -e /Applications ]]; then
 	VERSION=$(./getVersion.sh)
-  	rm /Applications/breakout-*.jar
 	cp breakout/target/breakout-$VERSION.jar /Applications
 	ln -sf /Applications/breakout-$VERSION.jar /Applications/Breakout
 fi
