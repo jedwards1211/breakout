dcce5df7510b6cf971fe55a468e51a39e7bed41c
Andy Edwards <jedwards@fastmail.com>
2019-02-10 12:24:32 -0700

    docs: create skeleton for documentation pages

---
+++ docs/.dude-wheres-my-module.js
@@ -0,0 +1,53 @@
+module.exports = async function configure() {
+  const path = require('path')
+  const { promisify } = require('util')
+  const glob = promisify(require('glob'))
+
+  const nodeModulesDir = path.join(__dirname, 'node_modules')
+
+  function assumeDefaultImports(files, options = {}) {
+    const transformIdentifier = options.transformIdentifier || (id => id)
+    const result = []
+    files.forEach(file => {
+      if (/index\.js$/.test(file)) file = path.dirname(file)
+      file = file.replace(/\.js$/, '')
+      const identifier = path.basename(file)
+      if (identifier[0] === '_' || /[^a-zA-Z0-9_]/.test(identifier)) return
+      result.push(
+        `import ${transformIdentifier(identifier, {
+          file,
+        })} from '${path.relative(nodeModulesDir, file)}'`
+      )
+    })
+    return result
+  }
+
+  async function globNodeModules(pattern) {
+    const files = await glob(path.join(nodeModulesDir, pattern))
+    return assumeDefaultImports(files)
+  }
+
+  const preferredImports = []
+  preferredImports.push(
+    ...assumeDefaultImports(
+      await glob(path.join(nodeModulesDir, '@material-ui/core/**/index.js'), {
+        ignore: [path.join(nodeModulesDir, '@material-ui/core/es/**')],
+      })
+    )
+  )
+  preferredImports.push(
+    ...assumeDefaultImports(
+      await glob(path.join(nodeModulesDir, '@material-ui/icons/*.js')),
+      { transformIdentifier: identifier => `${identifier}Icon` }
+    )
+  )
+  preferredImports.push(
+    ...assumeDefaultImports(
+      await glob(path.join(nodeModulesDir, '@material-ui/icons/*.js'))
+    )
+  )
+
+  return {
+    preferredImports,
+  }
+}
---
+++ docs/pages/index.js
@@ -0,0 +1,17 @@
+import * as React from 'react'
+
+import Paper from '@material-ui/core/Paper'
+import withStyles from '@material-ui/core/styles/withStyles'
+
+const homeStyles = theme => ({
+  root: {
+    margin: `${theme.spacing.unit * 2}px auto`,
+    maxWidth: 600,
+  },
+})
+
+const Home = ({ classes }) => (
+  <Paper className={classes.root}>This is a test!</Paper>
+)
+
+export default withStyles(homeStyles)(Home)
