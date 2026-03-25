769c642c38ebf79a15b0bf2f194f737217afa338
Andy Edwards <jedwards@fastmail.com>
2026-03-25 00:21:26 -0500

    ci: fix stupid issues

--- setVersion.mjs
+++ setVersion.mjs
@@ -1,13 +1,14 @@
 import fs from 'fs/promises'
+import { glob } from 'glob'
 
 const version = process.argv[2]
 
 console.error(`setting version to ${version}...`)
 
-for await (const file of fs.glob(['pom.xml', '*/pom.xml'])) {
+for (const file of await glob(['pom.xml', '*/pom.xml'])) {
   console.error(`updating ${file}...`)
   const content = await fs.readFile(file, 'utf8')
-  const replaced = content.replace('0.0.0-SNAPSHOT', version)
+  const replaced = content.replace(/0\.0\.0-SNAPSHOT/g, version)
   if (replaced !== content) {
     fs.writeFile(file, replaced, 'utf8')
     console.error(`wrote ${file}`)
